f35b1072e51dd350127cf224aa6f14b9d9101934
Roger Mendez <roger61087@gmail.com>
2021-12-18 11:41:30 -0500

    commit tree 1f4c4ed3ea497952edcf6fdb2b20bf4034ed4f3d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 class Dog
+def name= (value)
+    @this_dogs_name = value
+    
+end
+def name
+    @this_dogs_name
+    
+end
+
 
 end
+
+lassie = Dog.new
+lassie.name= "lassie"
+
+puts lassie.name= ("lassie")
+
+# binding.pry
